0ce6a77e47ea1c9f97fed0151094bece609ed603
maeschter <43206803+maeschter@users.noreply.github.com>
2018-09-13 17:59:08 +0200

    Delete DoppelterDatensatz.sql

--- info/DoppelterDatensatz.sql
+++
@@ -1,9 +0,0 @@
--- create a duplicated dataset from pk
-INSERT INTO album (filePath,fileName,icon,imageW,imageH,dateChanged,timeChanged,crc32,fileSize,keyWords,userComment,userDate)
-SELECT  filePath,fileName, icon,imageW,imageH,dateChanged,timeChanged,crc32,fileSize,keyWords,userComment,userDate
-FROM album WHERE pk = 8;
-
--- search duplicated datasets
-SELECT pk, filePath, fileName, fileSize, crc32 FROM album
-WHERE pk NOT IN 
-(SELECT X.pk FROM album AS X GROUP BY X.filePath, X.fileName, X.fileSize, X.crc32 HAVING COUNT(*)=1);
